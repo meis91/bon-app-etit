09ac187cc415a67bc036a168f931c86eacc7836f
meis91 <adrian.meis91@gmail.com>
2023-01-23 15:31:34 +0100

    Front- and Back-end architecture, initialized Spring Project in Back-End directory

---
+++ back-end/bae/src/main/java/com/codecool/bae/BonAppEtitApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonAppEtitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonAppEtitApplication.class, args);
+	}
+
+}
---
+++ back-end/bae/src/test/java/com/codecool/bae/BonAppEtitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.bae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonAppEtitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
