3ea88c7e32655e526a15cb21f1076081318ac6d3
Katharina Vogler <kvog82@gmail.com>
2023-01-24 13:00:01 +0100

    adding CrossOrigin Annotation to Controller

--- back-end/bon-app-etit/src/main/java/com/codecool/bonappetit/controller/RecipeController.java
+++ back-end/bon-app-etit/src/main/java/com/codecool/bonappetit/controller/RecipeController.java
@@ -1,10 +1,12 @@
 package com.codecool.bonappetit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/")
 public class RecipeController {
 
