c311fd5ea0281945938156ca52707817a6aba164
meis91 <adrian.meis91@gmail.com>
2023-01-24 14:54:30 +0100

    created componends folder in front-end

---
+++ front-end/bon-app-etit/src/components/Recipe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Recipe.propTypes = {
+
+};
+
+function Recipe(props) {
+
+
+    return (
+        <div></div>
+    );
+}
+
+export default Recipe;
