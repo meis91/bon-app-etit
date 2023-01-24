583713ecb0944f5351bd19469e6dd6600d8c9992
meis91 <adrian.meis91@gmail.com>
2023-01-24 14:41:20 +0100

    first axios test

--- front-end/bon-app-etit/src/App.js
+++ front-end/bon-app-etit/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+const baseURL = "/api";
 
 function App() {
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setPost(response.data);
+        });
+    }, []);
+
+    if (!post) return null;
+
   return (
     <div className="App">
       <h1>Bon App Etit</h1>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
     </div>
   );
 }
