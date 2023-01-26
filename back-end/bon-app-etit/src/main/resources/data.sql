db9c99a0326c37a999352d2402814bfe1b96e44a
eydu <Edu.remp33@gmail.com>
2023-01-26 14:23:36 +0100

    application.properties edited. schema.sql & data.sql created. App & Psql database not working yet.

---
+++ back-end/bon-app-etit/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ingredients(name, calories) VALUES ('apple', 100);
+INSERT INTO ingredients(name, calories) VALUES ('banana', 5);
+INSERT INTO ingredients(name, calories) VALUES ('potato', 10);
+INSERT INTO ingredients(name, calories) VALUES ('chocolate', 999);
