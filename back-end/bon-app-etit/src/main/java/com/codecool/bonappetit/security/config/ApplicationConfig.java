db5339acd2eef7c6c6a5ace09b4b58a3623e6c40
eydu <edu.remp33@gmail.com>
2023-02-21 19:16:39 +0100

    - JwtAuthenticationFilter now complete - spring-boot-starter-security added to pom.xml

--- back-end/bon-app-etit/src/main/java/com/codecool/bonappetit/security/config/ApplicationConfig.java
+++ back-end/bon-app-etit/src/main/java/com/codecool/bonappetit/security/config/ApplicationConfig.java
@@ -1,4 +1,24 @@
 package com.codecool.bonappetit.security.config;
 
+import com.codecool.bonappetit.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor //In case we wanna inject something
 public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService () {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+
+
 }
