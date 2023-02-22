512dd7dbb5c8d9e119ed754a885e793ecb685a35
eydu <Edu.remp33@gmail.com>
2023-02-22 09:20:06 +0100

    UserRepository, JwtAuthenticationFilter,

--- back-end/bon-app-etit/src/main/java/com/codecool/bonappetit/security/config/JwtAuthenticationFilter.java
+++ back-end/bon-app-etit/src/main/java/com/codecool/bonappetit/security/config/JwtAuthenticationFilter.java
@@ -1,4 +1,36 @@
 package com.codecool.bonappetit.security.config;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7); //Magic Number ?
+        userEmail = jwtService.extractUsername(jwt);
+    }
 }
